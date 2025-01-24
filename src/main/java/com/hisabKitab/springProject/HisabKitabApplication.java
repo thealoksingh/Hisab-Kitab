2ceb4b2f663c796b067d4bd019412ed1ece8a4c8
cyboravi <cyboravidell@gmail.com>
2025-01-24 06:11:13 +0530

    .env file bug fixed as now integrated as system environment variables

--- src/main/java/com/hisabKitab/springProject/HisabKitabApplication.java
+++ src/main/java/com/hisabKitab/springProject/HisabKitabApplication.java
@@ -1,17 +1,21 @@
 package com.hisabKitab.springProject;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HisabKitabApplication {
 
-    public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure().load();
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-        SpringApplication.run(HisabKitabApplication.class, args);
-        
-        System.out.println("Server Started Successfully..");
-    }
+	
+
+	public static void main(String[] args) {
+//        Dotenv dotenv = Dotenv.configure().load();
+//        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+		SpringApplication.run(HisabKitabApplication.class, args);
+
+		System.out.println("Server Started Successfully..");
+
+		
+	}
 }
