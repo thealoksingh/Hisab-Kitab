8d92fd80462ed9382fb0b273891094b1c109bb27
cyboravi <cyboravidell@gmail.com>
2025-02-04 21:21:48 +0530

    set as previously wy of fetching env variable

--- src/main/java/com/hisabKitab/springProject/HisabKitabApplication.java
+++ src/main/java/com/hisabKitab/springProject/HisabKitabApplication.java
@@ -3,17 +3,11 @@ package com.hisabKitab.springProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class HisabKitabApplication {
 
 
 	public static void main(String[] args) {
-		 Dotenv dotenv = Dotenv.configure()
-	                .directory("etc/secrets/")  // Point to the correct path
-	                .load();
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
 		SpringApplication.run(HisabKitabApplication.class, args);
 
