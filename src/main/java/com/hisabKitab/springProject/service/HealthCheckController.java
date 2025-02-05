9a352a0c1eaa6280376232b5535b078b33b7c699
cyboravi <cyboravidell@gmail.com>
2025-02-06 03:05:41 +0530

    added health check controller to check service is running and removed tracking of .class files

---
+++ src/main/java/com/hisabKitab/springProject/service/HealthCheckController.java
@@ -0,0 +1,16 @@
+package com.hisabKitab.springProject.service;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HealthCheckController {
+
+    @GetMapping("/ping")
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("Service is running");
+    }
+}
