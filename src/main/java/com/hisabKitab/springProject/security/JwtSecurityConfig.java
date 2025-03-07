c1a3a921ddb0c9619caeee80b38a9db0f8f4a7bb
cyboravi <cyboravidell@gmail.com>
2025-03-07 21:28:51 +0530

    added origin for all sites

--- src/main/java/com/hisabKitab/springProject/security/JwtSecurityConfig.java
+++ src/main/java/com/hisabKitab/springProject/security/JwtSecurityConfig.java
@@ -47,7 +47,7 @@ public class JwtSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000","https://hisab-kitab-business.netlify.app" )); // Frontend URL
+        configuration.setAllowedOrigins(List.of("http://localhost:3000","https://hisab-kitab-business.netlify.app","*" )); // Frontend URL
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
